07d3c85872c3d92fb9fab494a522c75301920b86
fmarciano69 <filippo.marciano@studenti.iispascal.it>
2026-05-08 10:26:18 +0200

    secondo commit

--- BusFlix/src/Controller/Gestore.java
+++ BusFlix/src/Controller/Gestore.java
@@ -4,12 +4,44 @@
  */
 package Controller;
 
+import Model.Film;
+import java.util.ArrayList;
+
 
 /**
  *
  * @author carvellip
  */
 public class Gestore {
+    ArrayList <Film> gestionefilm = new ArrayList();
     
+    public Film cercaPerCodice(String codice){
+        for(Film f : gestionefilm){
+            if(f.getCodice().equals(codice)){
+                return f;
+            }
+        }
+        return null;
+    }
+    
+    public ArrayList<Film> cercaPerCategoria(String categoria){
+        ArrayList<Film> gestcategoria = new ArrayList();
+        for(Film f : gestcategoria){
+            if(f.getCategoria().equals(categoria)){
+                gestcategoria.add(f);
+            }
+        }
+        return gestcategoria;
+    }
+    
+    public ArrayList<Film> cercaPerProtagonista(String protagonista){
+        ArrayList<Film> gestprotagonista = new ArrayList();
+        for(Film f : gestprotagonista){
+            if(f.getProtagonista().equals(protagonista)){
+                gestprotagonista.add(f);
+            }
+        }
+        return gestprotagonista;
+    }
    
 }
